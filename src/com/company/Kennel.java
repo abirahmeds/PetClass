b1b4b9a40f106ba2c87b641dc6cc1b0a8b254ec7
abir <43213194+abirahmeds@users.noreply.github.com>
2019-01-03 11:02:28 -0500

    Update Kennel.java

--- src/com/company/Kennel.java
+++ src/com/company/Kennel.java
@@ -2,23 +2,15 @@ package com.company;
 import java.util.ArrayList;
 
 public class Kennel {
-    public Kennel (cat, dog, loudDog)
-    {
-
-          private ArrayList<Pet> petList = new ArrayList<Pet>();
-
-          public void allSpeak () {
-          for (Pet p : petList) {
-             System.out.println(p.getName() + " says: " + p.speak());
-         }
-      }
-
-           public void addPet (Pet p){
-           petList.add(p);
-      }
-
-           public ArrayList<Pet> getPets () {
-           return this.petList;
+    private ArrayList<Pet> pets;
+    public Kennel(ArrayList<Pet> pets){
+        this.pets = pets;
+    }
+    public String allSpeak(){
+        String out = "";
+        for(int i = 0; i < pets.size(); i++){
+            out += pets.get(i).getName() + ":" + pets.get(i).speak() + "\n";
         }
+        return out;
     }
 }
