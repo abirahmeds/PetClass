45d07f3e5358e974e4f1c81e6e05c9bcbe17dbf7
abir <43213194+abirahmeds@users.noreply.github.com>
2019-01-03 10:59:08 -0500

    Update Runner.java

--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -1,16 +1,19 @@
 package com.company;
 
-    public class Runner {
-        public static void main(String[] args) {
-            Cat cat = new Cat("Cat");
-            Dog dog = new Dog("Dog");
-            LoudDog loudDog = new LoudDog("Loud Dog");
+import java.util.ArrayList;
 
-            System.out.println(cat.speak()); // → "meow"
-            System.out.println(dog.speak()); // → "wan"
-            System.out.println(loudDog.speak()); // → "wan wan"
-            new Kennel(cat, dog, loudDog).allSpeak();
-        }
-    }
-
+public class Runner {
+   public static void main(String[] args) {
+       Cat cat = new Cat("Cat");
+       Dog dog = new Dog("Dog");
+       LoudDog loudDog = new LoudDog("Loud Dog");
+       ArrayList<Pet> pets = new ArrayList<>();
+       pets.add(cat);
+       pets.add(dog);
+       pets.add(loudDog);
+       System.out.println(cat.speak()); // → "meow"
+       System.out.println(dog.speak()); // → "woof"
+       System.out.println(loudDog.speak()); // → "WOOF"
+       System.out.println(new Kennel(pets).allSpeak());
+   }
 }
